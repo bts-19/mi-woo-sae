23f7296f801ec4f7e2b8723a8905b93d7785744e
yesb0919 <wjkim5946@hanyang.ac.kr>
2019-05-17 11:08:29 +0900

    이미지 탐지 버전1

--- test_4.py
+++ test_4.py
@@ -40,17 +40,20 @@ while True :
 	if ( ret ):
 
 		gray = cv2.cvtColor( rgb, cv2.COLOR_BGR2GRAY ) # gray로 색상을 바꿈
-		gray = cv2.bilateralFilter( gray, 1, 10, 120 ) 
-		edges  = cv2.Canny( gray, 10, CANNY )
+		gray = cv2.bilateralFilter( gray, 1, 10, 120 ) # 이미지 필터링
+		edges  = cv2.Canny( gray, 10, CANNY )          # 가장자리 검춤 
 		kernel = cv2.getStructuringElement( cv2.MORPH_RECT, ( MORPH, MORPH ) )
 		closed = cv2.morphologyEx( edges, cv2.MORPH_CLOSE, kernel )
 		#contours, h = cv2.findContours( closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
-		contours, h = cv2.findContours( closed, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE )
+		contours, h = cv2.findContours( closed, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE ) #
+                #Contour
+		# 2번째 인자는 cv2.RETR_LIST는 계층 구조를 고려하지 않고 contour만 추출하는 방식
+		# 3번째 인자는 cv2.CHAIN_APPROX_NONE는 contour를 구성하는 모든점을 저장하는 방식
 
 		for cont in contours:
 
 			
-			if (cv2.contourArea( cont ) > 50000) & (cv2.contourArea( cont ) < 200000) :  #2000
+			if (cv2.contourArea( cont ) > 50000) & (cv2.contourArea( cont ) < 200000) :  #너무 큰 사각형이나 작은 사각형은 무시한다.
 				arc_len = cv2.arcLength( cont, True )
 				approx = cv2.approxPolyDP( cont, 0.02 * arc_len, True )
 				#다각형을 대상으로 꼭지점을 점점 줄여나가는 함수 
@@ -59,7 +62,7 @@ while True :
 				#parameter 3. 닫힌 곡선인지 열린 곡선인지
 
 				if ( len( approx ) == 4 ): #시긱형의 경우 len(approx) == 4
-					IS_FOUND = 1
+					IS_FOUND = 1 #사각형을 찾은 경우 IS_FOUND가 1로 설정
 					
 					pts_src = np.array( approx, np.float32 )
 					
@@ -70,7 +73,7 @@ while True :
 					h, status = cv2.findHomography( pts_src, pts_dst )
 					out = cv2.warpPerspective( rgb, h, ( int( _width + _margin * 2 ), int( _height + _margin * 2 ) ) ) 
 					
-					cv2.drawContours( rgb, [approx], -1, ( 255, 0, 0 ), 3) #255,0,0
+					cv2.drawContours( rgb, [approx], -1, ( 255, 0, 0 ), 3) # 255,0,0 으로 파랑색으로 경계를 설정
 
 				else : pass
 
